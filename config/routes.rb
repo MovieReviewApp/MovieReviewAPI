8f7d76b797b6ea21f4f1a0126c32a3cc72d11bd2
Annie <anne.bourgeois1@gmail.com>
2015-03-08 16:15:27 -0400

    added a nested route for reviews update under movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :reviews, only: [:create]
-  resources :movies, only: [:index, :show]
+  resources :movies, only: [:index]
+
+  resources :movies, only: [:show] do
+    resources :reviews, only: [:update]
+  end
 
   scope '/admin' do
     # Directs /admin/products/* to Admin::ProductsController
