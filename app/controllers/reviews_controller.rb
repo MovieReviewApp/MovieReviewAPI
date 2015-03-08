c88b122a998c6cfa766473194d68a57270c2f601
Annie <anne.bourgeois1@gmail.com>
2015-03-08 14:23:47 -0400

    Changed the review create route and the corresponding controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,9 @@ def index
 end
 
 def create
-  @movie = Movie.find(params[:movie_id])
+  # @movie = Movie.find(params[:movie_id])
   @review = Review.new(review_params)
-  @movie.reviews << @review
+  # @movie.reviews << @review
   if @review.save
     render json: @review, status: :created, location: @review
   else
@@ -18,12 +18,29 @@ def create
   end
 end
 
+# def update
+#   @review = Review.find(params[:id])
+#   if @review.update(review_params)
+#     render json: @review, status: :ok
+#   else
+#     render json: @review.errors, status: :unprocessable_entity
+#   end
+# end
+
 def update
   @review = Review.find(params[:id])
-  if @review.update(review_params)
-    render json: @review, status: :ok
+  if params[:movie_id]
+    @movie = Movie.find(params[:movie_id])
+    unless @movie.reviews.include? @review
+      @movie.reviews << @review
+    end
+    render json: @movie.reviews, status: :ok
   else
-    render json: @review.errors, status: :unprocessable_entity
+    if @review.update(review_params)
+      head :no_content
+    else
+      render json: @review.errors,status: :unprocessable_entity
+    end
   end
 end
 
@@ -35,7 +52,7 @@ end
 
 private
 def review_params
-  params.require(:review).permit(:comment, :star_rating, :username,:movie_id)
+  params.require(:review).permit(:comment, :star_rating, :username, :movie_id)
 end
 
 end
